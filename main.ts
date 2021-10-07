9f94c86b1d6c93fec8d8c2487f257897730280dd
deathau <death_au@hotmail.com>
2021-10-07 12:07:48 +1100

    Initial commit

---
+++ main.ts
@@ -0,0 +1,39 @@
+import { Plugin } from 'obsidian';
+
+export default class SnippetCommandsPlugin extends Plugin {
+
+  onload() {
+    this.enableCommands();
+  }
+
+  onunload() {
+  }
+
+  reloadCommands() {
+    this.unload()
+    this.load();
+  }
+
+  enableCommands() {
+    const customCss = (this.app as any).customCss;
+    console.log(customCss);
+    const knownSnippets = customCss.snippets;
+    console.log(`[Snippet Commands]: Loading commands for ${knownSnippets.length} snippets`, knownSnippets);
+    knownSnippets.forEach((snippet:string) => {
+      this.addCommand({
+        id: `snippet-command-${snippet}`,
+        name: `Toggle ${snippet}`,
+        callback: () => {
+          customCss.setCssEnabledStatus(snippet, !customCss.enabledSnippets.has(snippet));
+        }
+      });
+    });
+    this.addCommand({
+      id: `snippet-command-reload-all-snippets`,
+      name: `Reload snippet commands`,
+      callback: () => {
+        this.reloadCommands();
+      }
+    });
+  }
+}
